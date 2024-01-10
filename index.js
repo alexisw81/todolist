3923a93ce3791992d0314a7fd7b276ddb7b7af44
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-01-09 21:31:18 -0500

    Starting Javacript Pseudocode

---
+++ index.js
@@ -0,0 +1,11 @@
+//Define Variables
+
+//declare variable that holds the button selector.
+//create and array of buttons
+
+
+//loop through the buttons to create a click event
+//When task button is clicked, strike through the text
+for (let i = 1; i < task.length; i++) {
+
+}
