7f7f0a2119148b6e6af21947e1b55d63ed96430d
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-01-13 18:49:08 -0500

    Added task-complete event, working on user input

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //Define Variables
-
-//declare variable that holds the button selector.
-//create and array of buttons
+let task = document.getElementsByClassName("task");
+let taskInput = document.getElementById("task-input");
+let i;
 
 
 //loop through the buttons to create a click event
-//When task button is clicked, strike through the text
-for (let i = 1; i < task.length; i++) {
+for (i = 0; i < task.length; i++) {
+    task[i].addEventListener("click", function () {
+        //When task button is clicked, strike through the text
+        this.classList.toggle("task-done");
+    })
+};
 
-}
+//User Enters a task in input
+//when click enter 
+taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        //converts into button on task list
+    }
+})
