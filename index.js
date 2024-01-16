fba68a9447b5bc796568067f4cfa47c6b265eb5a
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-01-15 22:18:54 -0500

    Converted input value to an added task

--- index.js
+++ index.js
@@ -2,20 +2,37 @@
 let task = document.getElementsByClassName("task");
 let taskInput = document.getElementById("task-input");
 let i;
-
-
-//loop through the buttons to create a click event
-for (i = 0; i < task.length; i++) {
-    task[i].addEventListener("click", function () {
-        //When task button is clicked, strike through the text
-        this.classList.toggle("task-done");
-    })
-};
+let newTask;
+click();
 
 //User Enters a task in input
 //when click enter 
 taskInput.addEventListener("keypress", function (event) {
+    let value = taskInput.value;
     if (event.key === "Enter") {
-        //converts into button on task list
+        //converts input value into button on task list
+        newTask = document.createElement('button')
+        const newTaskValue = document.createTextNode(value);
+        newTask.appendChild(newTaskValue);
+        document.getElementById("list").appendChild(newTask);
+        newTask.classList.add("task");
+        //TODO Add new buttons to click event loop
+        console.log(newTask);
+        return (newTask);
+
     }
 })
+
+
+//loop through the buttons to create a click event
+function click() {
+    task.push(newTask);
+    for (i = 0; i < task.length; i++) {
+        console.log(task);
+        task[i].addEventListener("click", function () {
+            //When task button is clicked, strike through the text
+            this.classList.toggle("task-done");
+
+        })
+    };
+}
